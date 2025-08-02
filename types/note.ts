eadfeb6cc4a9e412ebbc9ee397671260679818be
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-02 20:06:20 +0300

    add type note

---
+++ types/note.ts
@@ -0,0 +1,16 @@
+export interface Note {
+  id: number;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  tag: string;
+}
+
+export interface NewNoteData {
+  title: string;
+  content: string;
+  tag: string;
+}
+
+
