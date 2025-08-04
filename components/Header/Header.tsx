85a5847760732d9a98e35fb91644546807b5e51c
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-04 12:06:29 +0300

    Add project structure without Loading and Error components

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,15 +1,20 @@
-<header className={css.header}>
-  <a href="/" aria-label="Home">
-    NoteHub
-  </a>
-  <nav aria-label="Main Navigation">
-    <ul className={css.navigation}>
-      <li>
-        <a href="/">Home</a>
-      </li>
-      <li>
-        <a href="/notes">Notes</a>
-      </li>
-    </ul>
-  </nav>
-</header>;
+import css from "@/components/Header/Header.module.css";
+import Link from "next/link";
+
+const Header = () => {
+  <header className={css.header}>
+    <Link href="/" aria-label="Home">
+      NoteHub
+    </Link>
+    <nav aria-label="Main Navigation">
+      <ul className={css.navigation}>
+        <li>
+          <Link href="/">Home</Link>
+        </li>
+        <li>
+          <Link href="/notes">Notes</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>;
+};
