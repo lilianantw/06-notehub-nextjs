569ef458c10f3539c016bb68a8ceb93e399caad4
liliana_toiunda <liliana.ntw@gmail.com>
2025-08-06 13:24:47 +0300

    fix notes/page

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -4,7 +4,7 @@ import {
   QueryClient,
 } from "@tanstack/react-query";
 import { fetchNoteById } from "@/lib/api";
-import NoteDetailsClient from "./NoteDetailsClient";
+import NoteDetailsClient from "./NoteDetails.client";
 import { notFound } from "next/navigation";
 
 export default async function NoteDetailsPage(props: {
